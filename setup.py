f4a76acb15890aa3e56929666828b7e803f30a1c
Alex Grönholm <alex.gronholm@nextday.fi>
2019-05-30 11:47:46 +0300

    Removed traces of Python 2 support

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ EXTRAS_REQUIRE = {
     "tests": ["pytest", "mock"],
     "lint": [
         "flake8==3.7.7",
-        'flake8-bugbear==19.3.0; python_version >= "3.5"',
+        'flake8-bugbear==19.3.0',
         "pre-commit==1.16.1",
     ],
 }
@@ -49,9 +49,7 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
